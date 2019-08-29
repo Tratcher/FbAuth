b9465a0b110f59cda77f636277341f1abe924be3
Chris R <Tratcher@outlook.com>
2019-08-29 12:07:49 -0700

    Initial commit

---
+++ FbAuthSample/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FbAuthSample.Controllers
+{
+    public class AccountController : Controller
+    {
+        public Task SignIn()
+        {
+            return HttpContext.ChallengeAsync(FacebookDefaults.AuthenticationScheme, new AuthenticationProperties()
+            {
+                RedirectUri = "/"
+            });
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect("/");
+        }
+    }
+}
